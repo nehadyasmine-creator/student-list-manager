c96f61a0977f11b95c8facfa8de7e527fc9ba648
BinomeA <nehadyasmine@gmail.com>
2026-02-19 17:13:14 +0100

    Ajout methodes

--- src/student_service.py
+++ src/student_service.py
@@ -1,10 +1,24 @@
+from student import Student
+
+
 class StudentService:
 
+    def __init__(self):
+        self.students = []
+
     def printList(self):
-        pass
+        print("Students:")
+        for student in self.students:
+            print(f"- {student.id}: {student.nom}")
 
     def addStudent(self, student):
-        pass
+        self.students.append(student)
+        print(f"Student '{student.nom}' added.")
 
     def removeStudent(self, student_id):
-        pass
+        for student in self.students:
+            if student.id == student_id:
+                self.students.remove(student)
+                print(f"Student with id '{student_id}' removed.")
+                return
+        print("Student not found.")
